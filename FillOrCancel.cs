88f0edbf6cec451523bffa8d7c1e9ec1d947f4ef
mikemet <mikemet@outlook.com>
2022-01-16 07:15:35 +0300

    filling all events handlers for buttons

--- FillOrCancel.cs
+++ FillOrCancel.cs
@@ -49,25 +49,163 @@ namespace SimpleDataApp
                 return true;
             }
         }
+        //=============================================================================================
 
+        /// <summary>
+        /// Executes a t-SQL SELECT statement to obtain order data for a specified
+        /// order ID, then displays it in the DataGridView on the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnFindByOrderID_Click(object sender, EventArgs e)
         {
+            if (IsOrderIDValid())
+            {
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                {
+                    // Define a t-SQL query string that has a parameter for orderID.
+                    const string sql = "SELECT * FROM Sales.Orders WHERE orderID = @orderID";
 
+                    // Create a SqlCommand object.
+                    using (SqlCommand sqlCommand = new SqlCommand(sql, connection))
+                    {
+                        // Define the @orderID parameter and set its value.
+                        sqlCommand.Parameters.Add(new SqlParameter("@orderID", SqlDbType.Int));
+                        sqlCommand.Parameters["@orderID"].Value = parsedOrderID;
+
+                        try
+                        {
+                            connection.Open();
+
+                            // Run the query by calling ExecuteReader().
+                            using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                            {
+                                // Create a data table to hold the retrieved data.
+                                DataTable dataTable = new DataTable();
+
+                                // Load the data from SqlDataReader into the data table.
+                                dataTable.Load(dataReader);
+
+                                // Display the data from the data table in the data grid view.
+                                this.dgvCustomerOrders.DataSource = dataTable;
+
+                                // Close the SqlDataReader.
+                                dataReader.Close();
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("The requested order could not be loaded into the form.");
+                        }
+                        finally
+                        {
+                            // Close the connection.
+                            connection.Close();
+                        }
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// Fills an order by calling the Sales.uspFillOrder stored
+        /// procedure on the database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnFillOrder_Click(object sender, EventArgs e)
         {
+            if (IsOrderIDValid())
+            {
+                // Create the connection.
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                {
+                    // Create command and identify it as a stored procedure.
+                    using (SqlCommand sqlCommand = new SqlCommand("Sales.uspFillOrder", connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
 
+                        // Add the order ID input parameter for the stored procedure.
+                        sqlCommand.Parameters.Add(new SqlParameter("@orderID", SqlDbType.Int));
+                        sqlCommand.Parameters["@orderID"].Value = parsedOrderID;
+
+                        // Add the filled date input parameter for the stored procedure.
+                        sqlCommand.Parameters.Add(new SqlParameter("@FilledDate", SqlDbType.DateTime, 8));
+                        sqlCommand.Parameters["@FilledDate"].Value = dtpFillDate.Value;
+
+                        try
+                        {
+                            connection.Open();
+
+                            // Execute the stored procedure.
+                            sqlCommand.ExecuteNonQuery();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("The fill operation was not completed.");
+                        }
+                        finally
+                        {
+                            // Close the connection.
+                            connection.Close();
+                        }
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// Cancels an order by calling the Sales.uspCancelOrder
+        /// stored procedure on the database.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCancelOrder_Click(object sender, EventArgs e)
         {
+            if (IsOrderIDValid())
+            {
+                // Create the connection.
+                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                {
+                    // Create the SqlCommand object and identify it as a stored procedure.
+                    using (SqlCommand sqlCommand = new SqlCommand("Sales.uspCancelOrder", connection))
+                    {
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
 
+                        // Add the order ID input parameter for the stored procedure.
+                        sqlCommand.Parameters.Add(new SqlParameter("@orderID", SqlDbType.Int));
+                        sqlCommand.Parameters["@orderID"].Value = parsedOrderID;
+
+                        try
+                        {
+                            // Open the connection.
+                            connection.Open();
+
+                            // Run the command to execute the stored procedure.
+                            sqlCommand.ExecuteNonQuery();
+                        }
+                        catch
+                        {
+                            MessageBox.Show("The cancel operation was not completed.");
+                        }
+                        finally
+                        {
+                            // Close connection.
+                            connection.Close();
+                        }
+                    }
+                }
+            }
         }
 
+        /// <summary>
+        /// Closes the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnFinishUpdates_Click(object sender, EventArgs e)
         {
-
+            Close();
         }
     }
 }

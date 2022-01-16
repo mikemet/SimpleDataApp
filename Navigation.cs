2fc0a10d6ac682c2e3261679a34cb33e768d1507
mikemet <mikemet@outlook.com>
2022-01-16 05:33:55 +0300

    add events handlers for every button

--- Navigation.cs
+++ Navigation.cs
@@ -16,5 +16,39 @@ namespace SimpleDataApp
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Opens the NewCustomer form as a dialog box,
+        /// which returns focus to the calling form when it is closed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnGoToAdd_Click(object sender, EventArgs e)
+        {
+            Form frm = new NewCustomer();
+            frm.Show();
+        }
+
+        /// <summary>
+        /// Opens the FillorCancel form as a dialog box. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGoToFillOrCancel_Click(object sender, EventArgs e)
+        {
+            Form frm = new FillOrCancel();
+            frm.ShowDialog();
+        }
+
+        /// <summary>
+        /// Closes the application (not just the Navigation form).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
